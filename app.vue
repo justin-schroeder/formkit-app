4671183d022b7486e9c52adfb01f85b78010bc52
Justin Schroeder <justin@formkit.com>
2024-03-22 14:47:34 -0400

    initial commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+interface DropdownOption {
+  label: string
+  value: string
+}
+const currencyOptions = ref<DropdownOption[]>([{ label: "Loading", value: "loading" }])
+const selectedCurrency = ref<string>("loading")
+</script>
+
+<template>
+  <FormKit
+    type="dropdown"
+    :value="selectedCurrency" 
+    :options="currencyOptions"
+  />
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@formkit/nuxt', '@nuxtjs/tailwindcss'],
+  formkit: {
+    autoImport: true
+  },
+  tailwindcss: {
+    config: {
+      content: ['./formkit.theme.ts']
+    }
+  },
+  typescript: {
+    typeCheck: true
+  }
+})
